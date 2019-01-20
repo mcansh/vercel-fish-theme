068e06b2194453edfc674491bc76282c00ea0e81
Logan McAnsh <logan@mcan.sh>
2019-01-19 23:50:15 -0500

    update prompt

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,50 +1,12 @@
-# function branch_is_dirty
-#   local STATUS=''
-#   local -a FLAGS
-#   FLAGS=('--porcelain')
-#   if [[ "$(command git config --get oh-my-zsh.hide-dirty)" != "1" ]]; then
-#     if [[ $POST_1_7_2_GIT -gt 0 ]]; then
-#       FLAGS+='--ignore-submodules=dirty'
-#     fi
-#     if [[ "$DISABLE_UNTRACKED_FILES_DIRTY" == "true" ]]; then
-#       FLAGS+='--untracked-files=no'
-#     fi
-#     STATUS=$(command git status ${FLAGS} 2> /dev/null | tail -n1)
-#   fi
-#   if [[ -n $STATUS ]]; then
-#     return 0
-#   else
-#     return 1
-#   fi
-# end
-
-# function git_prompt
-#   branch=`git_current_branch`
-#   if [ "$branch" = '' ]; then
-#     # not a git repo
-#     echo ''
-#   else
-#     if branch_is_dirty; then
-#       echo "$GIT_PROMPT_PREFIX$GIT_PROMPT_DIRTY$branch$GIT_PROMPT_SUFFIX"
-#     else
-#       echo "$GIT_PROMPT_PREFIX$GIT_PROMPT_CLEAN$branch$GIT_PROMPT_SUFFIX"
-#     fi
-#   fi
-# end
-
-
 function fish_prompt
-  set -l last_command_status $status
   set -l rest_color       (set_color normal)
   set -l logo             (set_color --bold white)▲$rest_color$rest_color
-  set -l ahead            "↑"
-  set -l behind           "↓"
-  set -l diverged         "⥄ "
   set -l dirty            "✖"
-  set -l none             "◦"
+  set -l none             "✔"
   set -l cwd
 
   set -l error_color      (set_color $fish_color_error ^/dev/null; or set_color red --bold)
+  set -l success_color    (set_color $fish_color_cwd ^/dev/null; or set_color green)
 
   if test "$theme_short_path" = 'yes'
     set cwd (basename (set_color --bold white prompt_pwd))
@@ -59,11 +21,14 @@ function fish_prompt
       set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
     end
 
-
-    echo -ns "$logo $cwd$repository_color at "(set_color --bold white) (git_branch_name) $reset_color " "
+    echo -ns "$logo $cwd at "(set_color --bold white) (git_branch_name) $reset_color " "
 
     if git_is_touched
-      echo -n -s "$error_color$dirty $reset_color"
+      echo -n -s $error_color $dirty $reset_color $reset_color " "
+    else
+      echo -n -s $success_color (git_ahead $none) $reset_color $reset_color " "
     end
+  else
+    echo -ns "$logo $cwd "
   end
 end
