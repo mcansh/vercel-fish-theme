41cf38867e147c35c0474d3ef0f1d901555d36f6
Logan McAnsh <logan@mcan.sh>
2019-01-20 20:48:51 -0500

    revert to working git branch code
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,4 +1,4 @@
-function git_prompt
+function git_status
   set -l dirty            "✖"
   set -l none             "✔"
   set -l error_color      (set_color $fish_color_error ^/dev/null; or set_color red --bold)
@@ -16,13 +16,7 @@ function dir
   set -l cwd
 
   if test "$theme_short_path" = 'yes'
-    if git_is_repo
-      set root_folder (command git rev-parse --show-toplevel ^/dev/null)
-      set parent_root_folder (dirname $root_folder)
-      set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
-    else
-      set cwd (basename (set_color --bold white prompt_pwd))
-    end
+    set cwd (basename (set_color --bold white prompt_pwd))
   else
     set cwd (prompt_pwd)
   end
@@ -32,15 +26,8 @@ function dir
       set root_folder (command git rev-parse --show-toplevel ^/dev/null)
       set parent_root_folder (dirname $root_folder)
       set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
-    else
-      set cwd (prompt_pwd)
     end
-  else
-    if test "$theme_short_path" = 'yes'
-      set cwd (basename (set_color --bold white prompt_pwd))
-    else
-      set cwd (prompt_pwd)
-    end
+    set cwd "$cwd at"(set_color --bold white) (git_branch_name) $reset_color
   end
 
   echo -ns "$cwd "
@@ -50,7 +37,7 @@ function fish_prompt
   set -l rest_color       (set_color normal)
   set -l logo             (set_color --bold white)▲ $rest_color$rest_color
   set -l cwd (dir)
-  set -l git (git_prompt)
+  set -l git (git_status)
 
   echo -ns "$logo$cwd$git"
 end
