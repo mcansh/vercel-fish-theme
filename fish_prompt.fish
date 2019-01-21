d4bcf0125cb9d27a10629f249623d800a6e429ef
Logan McAnsh <logan@mcan.sh>
2019-01-20 20:25:54 -0500

    more refactoring
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -5,15 +5,27 @@ function git_prompt
   set -l success_color    (set_color $fish_color_cwd ^/dev/null; or set_color green)
   if git_is_repo
     if git_is_touched
-      echo -ns "$error_color $dirty $reset_color"
+      echo -ns "$error_color$dirty$reset_color "
     else
-      echo -ns "$success_color $none $reset_color"
+      echo -ns "$success_color$none$reset_color "
     end
   end
 end
 
 function dir
   set -l cwd
+
+  if test "$theme_short_path" = 'yes'
+    if git_is_repo
+      set root_folder (command git rev-parse --show-toplevel ^/dev/null)
+      set parent_root_folder (dirname $root_folder)
+      set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
+    else
+      set cwd (basename (set_color --bold white prompt_pwd))
+    end
+  else
+    set cwd (prompt_pwd)
+  end
 
   if git_is_repo
     if test "$theme_short_path" = 'yes'
@@ -31,12 +43,14 @@ function dir
     end
   end
 
-  echo $cwd
+  echo -ns "$cwd "
 end
 
 function fish_prompt
   set -l rest_color       (set_color normal)
-  set -l logo             (set_color --bold white)▲$rest_color$rest_color
+  set -l logo             (set_color --bold white)▲ $rest_color$rest_color
+  set -l cwd (dir)
+  set -l git (git_prompt)
 
-  echo -n $logo (dir)(git_prompt)
+  echo -ns "$logo$cwd$git"
 end
